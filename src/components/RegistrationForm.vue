50fdfe306fed1473ed95422e177f30b481a616a7
Dmytro Demyanenko <dingofleep@gmail.com>
2021-02-22 11:58:11 +0200

    feat RegistrationForm

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -1,18 +1,47 @@
 <template>
   <form>
-    <input type="text">
-    <input type="text">
-    <input type="text">
-    <button>Register</button>
+    <input type="text" placeholder="Имя" v-model="name">
+    <input type="text" placeholder="Логин" v-model="username">
+    <input type="password" placeholder="Пароль" v-model="password">
+    <input type="password" placeholder="Повтор Пароля" v-model="repeatPassword">
+    <button @click="register">Зарегистрироваться</button>
+    <a href="/login">Войти</a>
   </form>
 </template>
 
 <script>
 export default {
-name: "RegistrationForm"
+  name: "RegistrationForm",
+  data() {
+    return {
+      name: "",
+      username: "",
+      password: "",
+      repeatPassword: ""
+    }
+  },
+  methods: {
+    register() {
+
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+form {
+  max-width: 600px;
+  margin: 0 auto;
+}
+input {
+  width: 100%;
+  margin: 0 0 10px 0;
+}
+button {
+  width: 100%;
+}
+a {
+  display: inline-block;
+  margin: 10px 0 0 0;
+}
 </style>
