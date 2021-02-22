c63bbc90d9d210de2f898e8696a7e810d2e9a0cd
Dmytro Demyanenko <dingofleep@gmail.com>
2021-02-22 11:59:52 +0200

    add LoginForm

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <form>
+    <input type="text" placeholder="Логин" v-model="username">
+    <input type="password" placeholder="Пароль" v-model="password">
+    <button @click="login">Войти</button>
+    <a href="/register">Зарегистрироваться</a>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "RegistrationForm",
+  data() {
+    return {
+      username: "",
+      password: "",
+    }
+  },
+  methods: {
+    login() {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+form {
+  max-width: 600px;
+  margin: 0 auto;
+}
+input {
+  width: 100%;
+  margin: 0 0 10px 0;
+}
+button {
+  width: 100%;
+}
+a {
+  display: inline-block;
+  margin: 10px 0 0 0;
+}
+</style>
